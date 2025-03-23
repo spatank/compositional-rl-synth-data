9527d435c405e6a25b6b5887b5648c2c193597c1
Shubhankar Patankar <49352524+spatank@users.noreply.github.com>
2025-03-23 18:13:43 +0530

    Consolidate scripts

--- scripts/train_diffusion_job.sh
+++ scripts/train_diffusion_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=diffusion_training
-#SBATCH --output=slurm/%A_%a_train_diffusion.out
-#SBATCH --mem=16G
+#SBATCH --output=slurm/%A_%a_train_conditional_diffusion.out
+#SBATCH --mem=128G
 #SBATCH --gpus=1
 #SBATCH --cpus-per-gpu=8
 #SBATCH --time=12:00:00
@@ -10,12 +10,12 @@
 
 source /home/spatank/compositional-rl-synth-data/venv/bin/activate
 
-python /home/spatank/compositional-rl-synth-data/scripts/train_diffusion.py \
-    --base_data_path /mnt/kostas-graid/datasets/spatank \
-    --base_results_folder /home/spatank/compositional-rl-synth-data/local_results/diffusion \
+python /home/spatank/compositional-rl-synth-data/scripts/train_conditional_diffusion.py \
+    --base_data_path /home/spatank/compositional-rl-synth-data/data \
+    --base_results_folder /home/spatank/compositional-rl-synth-data/results/diffusion \
     --gin_config_files /home/spatank/compositional-rl-synth-data/config/diffusion.gin \
+    --compositional False \
     --dataset_type expert \
-    --experiment_type smallscale \
-    --element IIWA \
+    --experiment_type default \
     --num_train 1 \
     --seed 42
