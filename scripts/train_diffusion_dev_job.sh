b1fc740b832da136cf2be9688cc1405829ed71bd
Shubhankar Patankar <49352524+spatank@users.noreply.github.com>
2025-03-24 22:00:43 +0530

    Edit single task training

--- scripts/train_diffusion_dev_job.sh
+++ scripts/train_diffusion_dev_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=diffusion_training
-#SBATCH --output=slurm/%A_%a_train_diffusion.out
-#SBATCH --mem=16G
+#SBATCH --output=slurm/%j_single_train1_seed0.out
+#SBATCH --mem=32G
 #SBATCH --gpus=1
 #SBATCH --cpus-per-gpu=8
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --partition=eaton-compute
 #SBATCH --qos=ee-med
 
@@ -14,7 +14,7 @@ python /home/spatank/compositional-rl-synth-data/scripts/train_diffusion_dev.py 
     --base_data_path /home/spatank/compositional-rl-synth-data/data \
     --base_results_folder /home/spatank/compositional-rl-synth-data/results/diffusion \
     --gin_config_files /home/spatank/compositional-rl-synth-data/config/diffusion.gin \
-    --denoiser monolithic \
+    --denoiser single \
     --task_list_path /home/spatank/compositional-rl-synth-data/offline_compositional_rl_datasets/_train_test_splits \
     --num_train 1 \
     --dataset_type expert \
